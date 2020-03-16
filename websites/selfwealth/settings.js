198fab9fc95f50cb85782ce6d0f54e21e41e9c94
Tom Dance <tom.dance@gmail.com>
2020-03-16 17:12:42 +1000

    Updated file paths and directories to accommodate multiple sites

---
+++ websites/selfwealth/settings.js
@@ -0,0 +1,8 @@
+// By Tom Dance <tom.dance@gmail.com>
+// Twitter: @tomdance
+
+$( document ).ready(function() {
+
+	// hide the portfolio value
+	$('span[data-bind="text: portfoliovalue"]').replaceWith( "<span style='color: #000000; background-color: #000000'>Don't worry about it :)</span>" );
+});
