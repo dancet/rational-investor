5cdd87a92a36d28adff2a160c80613475f772df3
Tom Dance <tomdance@Toms-MBA-Personal.local>
2024-08-16 14:10:13 +1000

    Update manifest to v3, update code to handle new sharesight dashboard styles

--- websites/sharesight/overview.js
+++ websites/sharesight/overview.js
@@ -7,14 +7,14 @@ chrome.runtime.getURL('utils.js');
 $( document ).ready(function() {
 
 	// hide the portfolio value in the header
-	$('[class^="HeaderBar__HeaderStats"]').replaceWith( "<span style='color: #000000; background-color: #000000; margin-left: auto; margin-right: 0'>Don't worry about it :)</span>" );
+	$('[class^="Statsstyled__StatsWrapper"]').replaceWith( "<span style='color: #000000; background-color: #000000; margin-left: auto; margin-right: 0'>Don't worry about it :)</span>" );
 
 	// hide the rest of the page items. They have to be selected from inside the ContentContainer
 	// class because it's reused on other pages
-	$("section[class^='ContentContainer-sc'] div[data-cy='Filters']").hide();
-	$("section[class^='ContentContainer-sc'] div[data-cy='ChartContainer']").hide();
-	$("section[class^='ContentContainer-sc'] div[class^='StatsBar__StatsContainer-sc']").hide();
-	$("section[class^='ContentContainer-sc'] div[data-cy='HoldingsContainer']").replaceWith("<h3>" + getRandomQuote() + "</h3>");
+	$("section[class^='Containerstyled__ContentContainer'] div[data-cy='Filters']").hide();
+	$("section[class^='Containerstyled__ContentContainer'] div[data-cy='ChartContainer']").hide();
+	$("section[class^='Containerstyled__ContentContainer'] div[class^='StatsBarstyled__StatsContainer']").hide();
+	$("section[class^='Containerstyled__ContentContainer'] div[data-cy='HoldingsContainer']").replaceWith("<h3>" + getRandomQuote() + "</h3>");
 
 });
 
